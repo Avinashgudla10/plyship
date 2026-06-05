829b0c479b3d4dad49c7bcf708ddce91b38caaa4
Avinash Gudla <avinashgudla10@gmail.com>
2026-06-05 22:54:04 +0530

    fix: add missing android.util.Log import in MainActivity

--- android/app/src/main/java/com/plyship/app/MainActivity.java
+++ android/app/src/main/java/com/plyship/app/MainActivity.java
@@ -20,6 +20,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
